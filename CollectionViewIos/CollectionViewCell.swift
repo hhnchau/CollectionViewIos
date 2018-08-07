1894114f28ecd52a4d9e9ef572745b9ae78b93be
hhnchau <kingpes@kingpess-mbp.mshome.net>
2018-08-07 11:17:55 +0700

    Initial Commit

---
+++ CollectionViewIos/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  CollectionViewIos
+//
+//  Created by Kingpes on 8/7/18.
+//  Copyright © 2018 Kingpes. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imgView: UIImageView!
+}
---
+++ CollectionViewIos/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  CollectionViewIos
+//
+//  Created by Kingpes on 8/7/18.
+//  Copyright © 2018 Kingpes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    @IBOutlet weak var collection: UICollectionView!
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
+        
+        cell.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        cell.imgView.image = UIImage(named: "logokingpes")
+        
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 200, height: 200)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collection.dataSource = self
+        collection.delegate = self
+    }
+}
+
